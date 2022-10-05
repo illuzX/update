91f0a30462e11e83bebcb8f4a83450317c17517b
illuzX <98640218+illuzX@users.noreply.github.com>
2022-10-05 18:42:22 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,16 +51,16 @@ async def start_message(bot, message):
 
 
 @illuzX.on_message(Worker.command("request")
-async def req(b, m):
+async def req(bot, message):
     if m.from_user.id not in ADMINS:
         await m.delete()
     msg = await m.reply("✒️ Eɴᴛᴇʀ Tʜᴇ Mᴏᴠɪᴇ Nᴀᴍᴇ\n⚠️ Uꜱᴇ Cᴏʀʀᴇᴄᴛ Gᴏᴏgle Sᴘᴇʟʟɪɴɢ ⚠️")
     
-@illuzX.on_message(Worker.command('search'))
-async def srch(bot, m):
-    if m.from_user.id not in ADMINS:
-        await m.delete()
-    msg = await m.reply("😂idk")
+#@illuzX.on_message(Worker.command('search'))
+#async def srch(bot, m):
+ #   if m.from_user.id not in ADMINS:
+     #   await m.delete()
+  #  msg = await m.reply("😂idk")
     
 @illuzX.on_message(Worker.private & Worker.command(["help"]))
 async def help(bot, message):
