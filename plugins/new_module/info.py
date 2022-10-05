3bddc956845c158a300bf1e8a27d0406e6dd2373
illuzX <98640218+illuzX@users.noreply.github.com>
2022-10-05 16:24:21 +0530

    update info.py

--- plugins/new_module/info.py
+++ plugins/new_module/info.py
@@ -13,6 +13,19 @@ from pyrogram.types import (
 #import Db Uri **To Know How Many Users (db)
 from plugins.database.users_chats_db import db
 from plugins.new_module.run_cb import humanbytes
+
+@illuzX.on_message(Worker.command("request")
+async def total(b, m):
+    if m.from_user.id not in ADMINS:
+        await m.delete()
+    msg = await m.reply("✒️ Eɴᴛᴇʀ Tʜᴇ Mᴏᴠɪᴇ Nᴀᴍᴇ\n⚠️ Uꜱᴇ Cᴏʀʀᴇᴄᴛ Gᴏᴏgle Sᴘᴇʟʟɪɴɢ ⚠️")
+    
+@illuzX.on_message(Worker.command('search'))
+async def total(bot, m):
+    if m.from_user.id not in ADMINS:
+        await m.delete()
+    msg = await m.reply("😂idk")
+    
 @illuzx.on_message(filters.command("status") & filters.user(config.ADMINS) & ~filters.edited)
 async def status_handler(_, m: Message):
     total, used, free = shutil.disk_usage(".")
@@ -25,12 +38,12 @@ async def status_handler(_, m: Message):
     total_users = await db.total_users_count ()
     files = await Media.count_documents()
     await m.reply_text(
-        text=f"**Total Disk Space:** {total} \n"
-             f"**Total Files:** {files} \n"
+        text=f"**Total Files:** {files} \n\n"
+             f"**Total Disk Space:** {total} \n"
              f"**Used Space:** {used}({disk_usage}%) \n"
              f"**Free Space:** {free} \n"
              f"**Cpu Usage:** {cpu_usage}% \n"
-             f"**Ram Usage:** {ram_usage}%\n\n"
+             f"**Ram Usage:** {ram_usage}%\n"
              f"**Total Users In db:** {total_users}`",
         parse_mode="Markdown",
         quote=True
