bad84d8fe2d2392a472eba7ba462f2eb007dd376
illuzX <98640218+illuzX@users.noreply.github.com>
2022-10-08 01:54:16 +0530

    update info.py

--- plugins/new_module/info.py
+++ plugins/new_module/info.py
@@ -6,14 +6,14 @@ import config
 #import client 
 from plugins.database.autofilter_db import Media
 from config import ADMINS
-from pyrogram import Client as illuzx, filters 
+from pyrogram import Client as illuzx, filters ,enums
 from pyrogram.types import (
     Message
 )
 #import Db Uri **To Know How Many Users (db)
 from plugins.database.users_chats_db import db
 from plugins.new_module.run_cb import humanbytes
-@illuzx.on_message(filters.command("status") & filters.user(config.ADMINS) & ~filters.edited)
+@illuzx.on_message(filters.command("status") & filters.user(config.ADMINS))
 async def status_handler(_, m: Message):
     total, used, free = shutil.disk_usage(".")
     total = humanbytes(total)
@@ -32,6 +32,6 @@ async def status_handler(_, m: Message):
              f"**Cpu Usage:** {cpu_usage}% \n"
              f"**Ram Usage:** {ram_usage}%\n"
              f"**Total Users In db:** {total_users}`",
-        parse_mode="Markdown",
+        enums.ParseMode.MARKDOWN,
         quote=True
     )
